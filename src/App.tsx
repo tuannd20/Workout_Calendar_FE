fe59867f657c3d7021d520d782610068b39f1acf
Nguyễn Duy Tuấn <nhanchunggiuadoi@gmail.com>
2022-10-21 17:57:11 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './pages/Home';
+
+function App() {
+  return (
+    <HomePage />
+  );
+}
+
+export default App;
